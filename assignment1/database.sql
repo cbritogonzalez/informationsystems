20c3a180a57caeaed0e7228286038fc762258c5a
Eduard <e.sabo@student.rug.nl>
2023-11-24 13:39:31 +0100

    added unnormalized db

--- assignment1/database.sql
+++ assignment1/database.sql
@@ -61,5 +61,19 @@ CREATE TABLE MonthlySalesReport (
     TotalSales DECIMAL(10, 2)
 );
 
+---Procedures
+CREATE TABLE customer_orders (
+    customer_id VARCHAR(5) PRIMARY KEY,
+    customer_name VARCHAR(50) NOT NULL,
+    order_details JSONB NOT NULL
+);
+
+-- Inserting data into the table
+INSERT INTO customer_orders (customer_id, customer_name, order_details)
+VALUES
+    ('C001', 'John Doe', '{"order_id": "0001", "book_titles": ["Database Fundamentals", "Advanced SQL"], "prices": [45.00, 60.00], "order_date": "2023-04-10", "delivery_address": "123 Elm St", "delivery_city": "Springfield", "delivery_zip": "12345"}'),
+    ('C002', 'Jane Smith', '{"order_id": "0002", "book_titles": ["Advanced SQL", "The Relational Model"], "prices": [60.00, 40.00], "order_date": "2023-04-11", "delivery_address": "456 Oak Ave", "delivery_city": "Springfield", "delivery_zip": "12345"}'),
+    ('C003', 'Emily Clark', '{"order_id": "0003", "book_titles": ["Database Fundamentals"], "prices": [45.00], "order_date": "2023-04-12", "delivery_address": "789 Pine Rd", "delivery_city": "Riverside", "delivery_zip": "67890"}');
+
 
 
