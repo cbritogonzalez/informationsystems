a9e26616ab5d25c821065c8e98a54d8feac9536c
Carlos Brito <carlos.britog25@gmail.com>
2023-11-27 00:16:09 +0100

    feat: add trigger to trigger 2

--- assignment1/question2/trigger2.sql
+++ assignment1/question2/trigger2.sql
@@ -16,3 +16,23 @@ CREATE TRIGGER prevent_deletion_customer_id
   ON customers
   FOR EACH ROW
   EXECUTE PROCEDURE prevent_deletion();
+
+
+CREATE OR REPLACE FUNCTION prevent_product_deletion()
+  RETURNS TRIGGER 
+  LANGUAGE PLPGSQL
+  AS
+$$
+BEGIN
+	IF EXISTS(SELECT BookTitle from orders where BookTitle = OLD.BookTitle) THEN
+		RAISE EXCEPTION 'The Book still has associated orders';
+	END IF;
+	RETURN OLD;
+END;
+$$
+
+CREATE TRIGGER prevent_deletion_product
+  BEFORE DELETE
+  ON product
+  FOR EACH ROW
+  EXECUTE PROCEDURE prevent_product_deletion();
